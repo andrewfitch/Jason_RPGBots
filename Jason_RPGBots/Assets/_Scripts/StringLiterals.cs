8ff70006e4bbe848c89561efa7c7a62a93e41242
andrewfitch <andyf@jamoc.com>
2023-03-08 12:27:30 -0800

    Set up 1D blend tree

---
+++ Jason_RPGBots/Assets/_Scripts/StringLiterals.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringLiterals
+{
+    public const string MouseX = "Mouse X";
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+    public const string Speed = "speed";
+}
--- Jason_RPGBots/Assets/_Scripts/ThirdPersonMovement.cs
+++ Jason_RPGBots/Assets/_Scripts/ThirdPersonMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
@@ -6,27 +7,34 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] float _turnSpeed = 1000f;
     [SerializeField] float  _moveSpeed = 5f;
     private Rigidbody _ridigbody;
+    private Animator _animator;
 
     private void Awake()
     {
         _ridigbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float mouseMovement = Input.GetAxis("Mouse X");
+        float mouseMovement = Input.GetAxis(StringLiterals.MouseX);
         transform.Rotate(0, mouseMovement * Time.deltaTime * _turnSpeed, 0);
     }
 
     //For physics updates
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis(StringLiterals.Horizontal);
+        float vertical = Input.GetAxis(StringLiterals.Vertical);
+        if(Input.GetKey(KeyCode.LeftShift)) 
+        {
+            vertical *= 2f;
+        }
 
         Vector3 velocity = new Vector3(horizontal, 0, vertical);
         velocity *= _moveSpeed * Time.fixedDeltaTime;
         Vector3 offset = transform.rotation * velocity;
         _ridigbody.MovePosition(transform.position + offset);
+        _animator.SetFloat(StringLiterals.Speed, vertical, 0.1f, Time.deltaTime);
     }
 }
