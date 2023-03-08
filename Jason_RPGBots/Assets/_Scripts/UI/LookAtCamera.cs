c8007928da0ef149deaa89a4d5e7e7b2f64baa4c
andrewfitch <andyf@jamoc.com>
2023-03-08 14:04:53 -0800

    Added nameplate for npc

---
+++ Jason_RPGBots/Assets/_Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        //Ensure that nameplate always faces the player
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180f, 0);
+    }
+}
